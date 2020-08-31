27942e06289dc3f380a38121d7cee58c0c10fedc
Henrique Martello <henrique.martello003@gmail.com>
2020-08-30 22:00:20 -0300

    Fix alinhamento textos "Painel Superior"

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -52,7 +52,7 @@ function Panel({ updateAt, onChange, data, country, getCovidData }) {
             <CardPanelContentStyled>
                 <div>
                     <Typography variant="h5" component="span" color="primary">COVID19<p/></Typography>
-                    <Typography variant="h6" component="span" color="primary">Painel Coronavírus - </Typography>
+                    <Typography variant="h6" component="span" color="primary">Painel Coronavírus<p/></Typography>
                     <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
                     <div className="pt-2">
                         <Select onChange={onChange} value={country}>
